da1376c5496506d281c56aa3fdfa74fdf7e727d9
VIn-2709 <vin.itku7277@gmail.com>
2025-06-05 13:05:45 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,8 @@ dotenv.config({});
 import connectDB from "./utils/db.js";
 import app from "./src/app.js";
 
-
-app.listen("https://task-management-theta-pied.vercel.app",()=>{
+PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>{
     connectDB();
     console.log("server is running ");
 })
