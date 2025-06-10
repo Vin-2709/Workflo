60471a681817e283320381ddfa0b9f3e79722a4b
VIn-2709 <vin.itku7277@gmail.com>
2025-06-10 16:48:13 +0530

    updated cards

--- Frontend/src/components/others/ActiveTask.jsx
+++ Frontend/src/components/others/ActiveTask.jsx
@@ -4,7 +4,7 @@ const ActiveTasks=({data})=>{
     const count = data.filter(task => task.status === "Pending").length;
 
     return (
-       <div className='hover:translate-y-2 transition-transform duration-300 hover:cursor-pointer bg-blue-100  border-t-6 border-green-500 w-[24%] h-[130px] shadow-lg  text-green-600 text-center rounded-2xl p-2 text-[27px] font-semibold  shadow-blue-900'>
+       <div className='hover:translate-y-2 transition-transform duration-300 hover:cursor-pointer bg-white  border-t-6 border-green-500 w-[24%] h-[130px] shadow-lg  text-green-600 text-center rounded-2xl p-2 text-[27px] font-semibold  shadow-blue-900'>
             <h1>Active Tasks</h1>
             <h1 className='font-bold mt-3 text-[30px]'>{count}</h1>
         </div>
--- Frontend/src/components/others/CompletedTask.jsx
+++ Frontend/src/components/others/CompletedTask.jsx
@@ -5,7 +5,7 @@ const CompletedTask=({data})=>{
   
 
     return (
-        <div className='hover:translate-y-2 transition-transform duration-300  hover:cursor-pointer border-t-6 border-yellow-400 bg-blue-100  w-[24%] h-[130px] shadow-lg  text-yellow-600 text-center rounded-2xl p-2 text-[27px] font-semibold  shadow-blue-900'>
+        <div className='hover:translate-y-2 transition-transform duration-300  hover:cursor-pointer border-t-6 border-yellow-400 bg-white  w-[24%] h-[130px] shadow-lg  text-yellow-600 text-center rounded-2xl p-2 text-[27px] font-semibold  shadow-blue-900'>
             <h1>Completed Tasks</h1>
             <h1 className='font-bold mt-3 text-[30px]'>{count}</h1>
         </div>
