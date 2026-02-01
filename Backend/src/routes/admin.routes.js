a09ab172ea1f550a89b45cdb661c942503aa153d
Vineet <vin.itku7277@gmail.com>
2026-02-01 22:23:51 +0530

    Remove authentication middleware from admin routes

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -5,9 +5,9 @@ import { Uploadmulter } from '../middlewares/multer.js';
 
 const router = express.Router();
 
-router.get('/gettask/:id', isLogged, getAdminTasks);
-router.post('/:id/assigntask', isLogged, Uploadmulter.array("files", 5), assignTask);
-router.get('/:id', isLogged, getAdminProfile);
+router.get('/gettask/:id', getAdminTasks);
+router.post('/:id/assigntask', Uploadmulter.array("files", 5), assignTask);
+router.get('/:id', getAdminProfile);
 
 
 export default router;
