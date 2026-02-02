f0944fd35bce40cdfdadc3a12b711ba54eb78250
Vineet <vin.itku7277@gmail.com>
2026-02-02 12:08:08 +0530

    Add delete file route to upload routes

--- Backend/src/routes/upload.routes.js
+++ Backend/src/routes/upload.routes.js
@@ -2,9 +2,12 @@ import express from 'express';
 import { adminUpload, employeeUpload } from '../controllers/upload.controller.js';
 import { isLogged } from '../middlewares/auth.middleware.js';
 import { Uploadmulter } from '../middlewares/multer.js';
+import { adminUpload, employeeUpload, deleteFile } from '../controllers/upload.controller.js';
+
 
 const router = express.Router();
 
+router.delete("/delete-file/:taskId", isLogged, deleteFile);
 router.post("/admin-upload/:taskId", isLogged, Uploadmulter.array("files", 5), adminUpload);
 router.post("/employee-upload/:taskId", isLogged, Uploadmulter.array("files", 5), employeeUpload);
 
